3f460d61769071fdf03d5d2899f98ec44134bc30
EscapeFromJava <reske.artur@gmail.com>
2022-08-15 00:48:40 +0300

    v 1.1 1) в методах listUsers() и getUserByCar() сделай так, чтобы IDEA не подсвечивала желтым. @SuppressWarnings больше нигде не используй. +

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -2,36 +2,35 @@ package hiber.dao;
 
 import hiber.model.User;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    public List<User> listUsers() {
+        Query<?> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return (List<User>) query.getResultList();
+    }
 
-   @Override
-   public User getUserByCar(String carModel, int carSeries) {
-      TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("SELECT u FROM User AS u INNER JOIN u.userCar AS c WHERE c.model = :carModel AND c.series = :carSeries");
-      query.setParameter("carModel", carModel);
-      query.setParameter("carSeries", carSeries);
-      return query.getSingleResult();
-   }
+    @Override
+    public User getUserByCar(String carModel, int carSeries) {
+        Query<?> query = sessionFactory.getCurrentSession().createQuery("SELECT u FROM User AS u INNER JOIN u.userCar AS c WHERE c.model = :carModel AND c.series = :carSeries");
+        query.setParameter("carModel", carModel);
+        query.setParameter("carSeries", carSeries);
+        return (User) query.getSingleResult();
+    }
 
 }
